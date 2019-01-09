3ff2a09bedc06255814d86a9c658058571af3d72
battle_steed <battle_steed@qq.com>
2019-01-09 18:04:31 +0800

    修复事务测试不通过bug

--- hibernateMaster/src/test/java/steed/hibernatemaster/test/TestTransaction.java
+++ hibernateMaster/src/test/java/steed/hibernatemaster/test/TestTransaction.java
@@ -7,41 +7,13 @@ import steed.hibernatemaster.util.DaoUtil;
 import steed.util.base.BaseUtil;
 
 public class TestTransaction extends SteedTest{
-
+/*
 	@Test
 	public void queryTransactionID(){
 		DaoUtil.listOne(new Student());//.delete();
 		Object uniqueResult = DaoUtil.createSQLQuery(null, new StringBuffer("SELECT TRX_ID FROM INFORMATION_SCHEMA.INNODB_TRX  WHERE TRX_MYSQL_THREAD_ID = CONNECTION_ID()")).uniqueResult();
 		BaseUtil.out(uniqueResult);
-	}
-	
-	@Test
-	public void testCommitFail(){
-		Student student = new Student();
-		student.setId("TestTransaction.testCommitFail");
-		student.delete();
-		student.save();
-		Student student2 = new Student();
-		student2.setId("TestTransaction.testCommitFail2");
-		student2.delete();
-		student2.save();
-		DaoUtil.managTransaction();
-		DaoUtil.closeSessionNow();
-		
-		DaoUtil.getSession().createSQLQuery("update student set name='22222222222' where id = 'TestTransaction.testCommitFail2'").executeUpdate();
-		student = new Student();
-		student.setId("TestTransaction.testCommitFail");
-		student.save();
-		
-		DaoUtil.managTransaction();
-		DaoUtil.closeSessionNow();
-		student2 = new Student();
-		student2.setName("22222222222");
-		
-		assert(DaoUtil.getCount(student2) == 0);
-		student.delete();
-		student2.delete();
-	}
+	}*/
 	
 	@Test
 	public void testFlushRollback(){
@@ -65,6 +37,37 @@ public class TestTransaction extends SteedTest{
 		DaoUtil.closeSessionNow();
 		
 		assert(DaoUtil.deleteByQuery(student) == 0);
+	}
+	
+	@Test
+	public void testCommitFail(){
+		DaoUtil.managTransaction();
+		DaoUtil.closeSessionNow();
+		Student student = new Student();
+		student.setId("TestTransaction.testCommitFail");
+		student.delete();
+		student.save();
+		Student student2 = new Student();
+		student2.setId("TestTransaction.testCommitFail2");
+		student2.delete();
+		student2.save();
+		DaoUtil.managTransaction();
+		DaoUtil.closeSessionNow();
+		
+		DaoUtil.getSession().createSQLQuery("update student set name='22222222222' where id = 'TestTransaction.testCommitFail2'").executeUpdate();
+		student = new Student();
+		student.setId("TestTransaction.testCommitFail");
+		student.save();
+		
+		DaoUtil.managTransaction();
+		DaoUtil.closeSessionNow();
+		student2 = new Student();
+		student2.setName("22222222222");
+		
+		assert(DaoUtil.getCount(student2) == 0);
+		student.delete();
+//		student2.setId("TestTransaction.testCommitFail2");
+		student2.delete();
 	}
 	
 	
