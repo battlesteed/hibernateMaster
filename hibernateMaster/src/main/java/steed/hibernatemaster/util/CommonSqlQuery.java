12a0f4265da7e79fc6ad4e7c755a78fb8b1502b9
battlesteed <battlesteed@battlesteed>
2024-04-02 22:54:04 +0800

    2

--- hibernateMaster/src/main/java/steed/hibernatemaster/util/CommonSqlQuery.java
+++ hibernateMaster/src/main/java/steed/hibernatemaster/util/CommonSqlQuery.java
@@ -1,6 +1,15 @@
 package steed.hibernatemaster.util;
 
+/**
+ * 常用的sql查询功能
+ * @author battlesteed
+ *
+ */
 public class CommonSqlQuery {
+	/**
+	 * 查询数据库版本
+	 * @return
+	 */
 	public static String getDatabaseVersion() {
 		return (String) DaoUtil.getSession().createSQLQuery("SELECT VERSION();").uniqueResult();
 	}
